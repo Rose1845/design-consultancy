eab0523c95a04e86f21ca15493bbc25784e798cb
Rose Odhiambo <odhiamborose466@gmail.com>
2023-01-02 15:39:48 +0300

    adding icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import {FaTwitter,FaLinkedin, FaGithub} from "react-icons/fa"
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div className='footer'>
         <div className='flex'>
         <p className='text-center text-white'>&copy; copyright {new Date().getFullYear()}</p>
-        <div className='row'>
+        <div className='col'>
+        <Link>
+        <FaTwitter/>
+        </Link>
+       <Link>
+       <FaLinkedin/>
+       </Link>
+       <Link>
+       <FaGithub/>
+       </Link>
           
         </div>
         <p className='text-white text-center'>rosevilla designs</p>
