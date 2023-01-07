71fb69714370e49d3548a725aa0f25fa3b4ff8ec
Rose Odhiambo <odhiamborose466@gmail.com>
2023-01-07 13:08:33 +0300

    removed some file

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD-wWjwNXrYTmzNXMMNNgdFv8jCP2w-_dw",
-//   authDomain: "design-2fd9c.firebaseapp.com",
-//   projectId: "design-2fd9c",
-//   storageBucket: "design-2fd9c.appspot.com",
-//   messagingSenderId: "170669809721",
-//   appId: "1:170669809721:web:485c8667624f8b49676de8"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
